de44dfc8b5ba857769e2ee6840a1c47fad7d6d54
aliestarten <aliestarten@gmail.com>
2016-11-17 13:37:47 +0700

    deploy: 2016-11-17 13:37:47

--- frontend/react/confirm.jsx
+++ frontend/react/confirm.jsx
@@ -1,6 +1,7 @@
 import React, {PropTypes} from 'react';
 import {confirmable, createConfirmation} from 'react-confirm';
-import Modal, {customModalStyles} from 'react-modal';
+import Modal from 'react-modal';
+import {customModalStyles} from './modal';
 
 const Dialog = ({show, proceed, dismiss, cancel, confirmation, options}) => {
     return(
@@ -8,15 +9,15 @@ const Dialog = ({show, proceed, dismiss, cancel, confirmation, options}) => {
             isOpen={show}
             style={customModalStyles}
             onRequestClose={dismiss}>
-                <div className="row row-centered">
-                    <div className="col-md-12 col-centered">
+                <div className="row row-centered modal_block">
+                    <div className="col-md-12 col-centered modal_header">
                         {confirmation}
                     </div>
-                    <div className="col-md-12 col-centered">
-                        <div className="col-md-2 col-centered">
+                    <div className="col-md-12 col-centered modal_body">
+                        <div className="col-md-3 col-centered">
                             <button className="btn btn-danger" onClick={() => cancel('arguments will be passed to the callback')}>Отмена</button>
                         </div>
-                        <div className="col-md-2 col-centered">
+                        <div className="col-md-3 col-centered">
                             <button className="btn btn-success" onClick={() => proceed('same as cancel')}>Принять</button>
                         </div>
                     </div>

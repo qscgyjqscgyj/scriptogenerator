4abc183e5a3b7deb7da9171fc44ab1d540fedc69
aliestarten <aliestarten@gmail.com>
2016-12-21 19:19:22 +0700

    deploy: 2016-12-21 19:19:22

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -24,22 +24,22 @@ def cloneTreeRelations(mainObject_pk, cloneObject_pk, app_name, model_name, recu
                 cloneObject.opened = False
                 cloneObject.save()
 
-            # IF THE LINK OBJECT
-            if cloneObject.__class__.__name__ == 'Link':
-                if cloneObject.to_link:
-                    print('to_link (' + str(cloneObject.pk) + ') script before: ' + str(cloneObject.to_link.category.table.table.script.pk))
-                    try:
-                        to_link = cloneObject.__class__.objects.get(parent__pk=cloneObject.to_link.pk,
-                                                                    category__table__table__script__pk=cloneObject.category.table.table.script.pk)
-                        cloneObject.to_link = to_link
-                        cloneObject.save()
-                        print('found')
-                        print('to_link (' + str(cloneObject.pk) + ') script after: ' + str(cloneObject.to_link.category.table.table.script.pk))
-                    except ObjectDoesNotExist:
-                        print('not found')
-                        time.sleep(1)
-                        recursive_iteration += 1
-                        cloneTreeRelations.delay(mainObject_pk, cloneObject_pk, app_name, model_name, recursive_iteration)
+            # # IF THE LINK OBJECT
+            # if cloneObject.__class__.__name__ == 'Link':
+            #     if cloneObject.to_link:
+            #         print('to_link (' + str(cloneObject.pk) + ') script before: ' + str(cloneObject.to_link.category.table.table.script.pk))
+            #         try:
+            #             to_link = cloneObject.__class__.objects.get(parent__pk=cloneObject.to_link.pk,
+            #                                                         category__table__table__script__pk=cloneObject.category.table.table.script.pk)
+            #             cloneObject.to_link = to_link
+            #             cloneObject.save()
+            #             print('found')
+            #             print('to_link (' + str(cloneObject.pk) + ') script after: ' + str(cloneObject.to_link.category.table.table.script.pk))
+            #         except ObjectDoesNotExist:
+            #             print('not found')
+            #             time.sleep(1)
+            #             recursive_iteration += 1
+            #             cloneTreeRelations.delay(mainObject_pk, cloneObject_pk, app_name, model_name, recursive_iteration)
 
             relations = type(mainObject)._meta.get_all_related_objects_with_model()
             for relate in relations:
@@ -60,7 +60,7 @@ def cloneTreeRelations(mainObject_pk, cloneObject_pk, app_name, model_name, recu
         except ObjectDoesNotExist:
             time.sleep(1)
             recursive_iteration += 1
-            cloneTreeRelations.delay(mainObject_pk, cloneObject_pk, app_name, model_name, recursive_iteration)
+            return cloneTreeRelations.delay(mainObject_pk, cloneObject_pk, app_name, model_name, recursive_iteration)
     else:
         return False
 
@@ -72,17 +72,30 @@ def clone_save_links(clone_script_pk, current_script_links_count, recursive_iter
         clone_script = Script.objects.get(pk=clone_script_pk)
         clone_links = clone_script.links(parent=True)
 
-        while len(clone_links) < current_script_links_count:
+        if len(clone_links) < current_script_links_count:
             time.sleep(1)
             print(len(clone_links))
             print(current_script_links_count)
             print('Iteration: ' + str(recursive_iteration))
             print('---------------------')
-            return clone_save_links.delay(clone_script_pk, current_script_links_count, recursive_iteration)
-
-        for link in clone_links:
-            link.clone_save()
-        clone_script.active = True
-        return clone_script.save()
+            clone_save_links.delay(clone_script_pk, current_script_links_count, recursive_iteration)
+        else:
+            for link in clone_links:
+                if link.to_link:
+                    print('to_link (' + str(link.pk) + ') script before: ' + str(link.to_link.category.table.table.script.pk))
+                    try:
+                        to_link = Link.objects.get(parent__pk=link.to_link.pk, category__table__table__script__pk=link.category.table.table.script.pk)
+                        link.to_link = to_link
+                        link.save()
+                        print('found')
+                        print('to_link (' + str(link.pk) + ') script after: ' + str(link.to_link.category.table.table.script.pk))
+                    except ObjectDoesNotExist:
+                        print('not found')
+                        time.sleep(1)
+                        recursive_iteration += 1
+                        clone_save_links.delay(clone_script_pk, current_script_links_count, recursive_iteration)
+                link.clone_save()
+            clone_script.active = True
+            return clone_script.save()
     else:
         return False

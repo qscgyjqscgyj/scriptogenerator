a8caa9b79ea95496a64c20bc5cd27e230e9bdb50
aliestarten <aliestarten@gmail.com>
2016-12-26 20:55:04 +0700

    deploy: 2016-12-26 20:55:04

--- payment/admin.py
+++ payment/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from payment.models import *
 
+
+class UserPaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'payed')
+
+
 admin.site.register(Balance)
 admin.site.register(Bonus)
-admin.site.register(UserPayment)
+admin.site.register(UserPayment, UserPaymentAdmin)
 admin.site.register(LocalPayment)
 

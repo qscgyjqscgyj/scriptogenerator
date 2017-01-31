fced42ad33f4c6bb1c92540cbd87382e481a128f
aliestarten <aliestarten@gmail.com>
2017-01-31 15:36:50 +0700

    add fake username max_length

---
+++ users/migrations/0016_auto_20170131_1530.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0015_username_max_length'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='customuser',
+            name='username',
+            field=models.CharField(error_messages={'unique': 'A user with that username already exists.'}, max_length=30, validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username. This value may contain only letters, numbers and @/./+/-/_ characters.', 'invalid')], help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', unique=True, verbose_name='username'),
+        ),
+    ]

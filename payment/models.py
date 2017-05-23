0c9bb3cbdfba159a8248665398e0f8483e533a57
aliestarten <aliestarten@gmail.com>
2017-05-23 21:22:31 +0700

    deploy: 2017-05-23 21:22:31

--- payment/models.py
+++ payment/models.py
@@ -1,19 +1,23 @@
+# -*- coding: utf-8 -*-
+import datetime
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from main.models import Script
 from payment.tasks import recount_balance
 from users.models import CustomUser
 
 
 class UserPayment(models.Model):
+    name = models.CharField(default='Пополнение счета', max_length=1024)
     user = models.ForeignKey(CustomUser, related_name='user_payment_user_custom_user')
     sum = models.FloatField(default=0)
     total_sum = models.FloatField(default=0)
-    date = models.DateTimeField(auto_now_add=True)
     payment_data = models.TextField(blank=True, null=True)
     payed = models.DateTimeField(blank=True, null=True)
     promotion = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         if not self.total_sum or self.total_sum < self.sum:
@@ -40,3 +44,34 @@ class LocalPayment(models.Model):
 
     class Meta:
         ordering = ('-pk',)
+
+
+class UserScriptDelegationAccess(models.Model):
+    user = models.ForeignKey(CustomUser, related_name='user_script_delegation_access_custom_user')
+    delegated_user = models.ForeignKey(CustomUser, related_name='user_script_delegation_access_delegated_user_custom_user', blank=True, null=True)
+    delegated_script = models.ForeignKey(Script, related_name='user_script_delegation_access_delegated_script_script', blank=True, null=True)
+    delegated = models.BooleanField(default=False)
+    payed = models.DateTimeField(blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.user.__unicode__()
+
+
+class UserOfflineScriptExportAccess(models.Model):
+    user = models.ForeignKey(CustomUser, related_name='user_offline_script_export_access_custom_user')
+    script = models.ForeignKey(Script, on_delete=models.SET_NULL, blank=True, null=True)
+    script_data = models.TextField(blank=True, null=True)
+    exported = models.BooleanField(default=False)
+    unlim_months = models.IntegerField(blank=True, null=True)
+    payed = models.DateTimeField(blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.user.__unicode__()
+
+    def unlime_datetime_expires(self):
+        return self.payed + datetime.timedelta(self.unlim_months * 365 / 12)
+
+    def unlim_is_expired(self):
+        return True if datetime.datetime.now() < self.unlime_datetime_expires() else False
--- payment/views.py
+++ payment/views.py
@@ -3,6 +3,7 @@ import datetime
 import urllib
 
 import pytz
+from constance import config
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
@@ -14,12 +15,13 @@ import json
 import hashlib
 from dateutil.tz import tzlocal
 
-from payment.models import UserPayment
+from payment.models import UserPayment, UserScriptDelegationAccess, UserOfflineScriptExportAccess
 from payment.serializers import UserPaymentSerializer
 from django.core.mail import send_mail
 
 from payment.tasks import recount_balance
 from scripts.settings import YANDEX_SHOPID, YANDEX_SHOPPASSWORD
+from users.models import CustomUser
 
 
 class PaymentView(View):
@@ -131,3 +133,117 @@ class PaymentFailView(View):
         if test_mode and test_mode == 'test':
             return JSONResponse({'success': True, 'test': True})
         return JSONResponse({'success': True})
+
+
+class GetproffExternalPayment:
+    PRODUCT_ID = None
+
+    def get_positions(self, positions):
+        if ' x ' in positions and self.PRODUCT_ID in positions:
+            return positions.split(' x ')[0]
+        return positions
+
+    def get_products_count(self, positions):
+        if ' x ' in positions and self.PRODUCT_ID in positions:
+            return int(positions.split(' x ')[1])
+        return 1
+
+    def get_multiplier(self, positions):
+        splited_positions = self.get_positions(positions).split(self.PRODUCT_ID)
+        return 1 if (len(splited_positions) == 2 and splited_positions[1] == '') else int(splited_positions[1][1:])
+
+
+# data examples:
+# {u'positions': [u'pay.user.6 x 7'], u'type': [u'ext'], u'email': [u'sky-life@inbox.ru']}
+# {u'positions': [u'pay.user'], u'type': [u'ext'], u'email': [u'sky-life@inbox.ru']}
+class PaymentForUserAccesses(View, GetproffExternalPayment):
+    PRODUCT_ID = 'pay.user'
+
+    def get(self, request, *args, **kwargs):
+        positions = request.GET.get('positions')
+        email = request.GET.get('email')
+        if email:
+            if positions and self.PRODUCT_ID in positions:
+                users_multiplier = self.get_multiplier(positions)
+                months_multiplier = self.get_products_count(positions)
+                payment_sum = config.PAYMENT_PER_USER * users_multiplier * months_multiplier
+                payment = UserPayment(
+                    name='Пополнение счета. Пользователей: %(product_multiplier)s, месяцев: %(months_multiplier)s' % dict(
+                        product_multiplier=str(users_multiplier),
+                        months_multiplier=str(months_multiplier)
+                    ),
+                    user=CustomUser.objects.get(username=email),
+                    sum=payment_sum,
+                    total_sum=payment_sum,
+                    payed=datetime.datetime.now()
+                )
+                payment.save()
+                return JSONResponse({'status': 'success'}, status=200)
+            return JSONResponse({'status': 'error', 'message': 'Positions does not support with this method'}, status=500)
+        return JSONResponse({'status': 'error', 'message': 'Email is required'}, status=500)
+
+
+# data examples
+# {u'positions': [u'delegate.script x 8'], u'type': [u'ext'], u'email': [u'sky-life@inbox.ru']}
+# {u'positions': [u'delegate.script'], u'type': [u'ext'], u'email': [u'sky-life@inbox.ru']}
+class PaymentForScriptDelegation(View, GetproffExternalPayment):
+    PRODUCT_ID = 'delegate.script'
+
+    def get(self, request, *args, **kwargs):
+        positions = request.GET.get('positions')
+        email = request.GET.get('email')
+        if email:
+            if positions and self.PRODUCT_ID in positions:
+                products_count = self.get_products_count(positions)
+                for i in range(products_count):
+                    UserScriptDelegationAccess.objects.create(
+                        user=CustomUser.objects.get(username=email),
+                        payed=datetime.datetime.now()
+                    )
+                return JSONResponse({'status': 'success'}, status=200)
+            return JSONResponse({'status': 'error', 'message': 'Positions does not support with this method'}, status=500)
+        return JSONResponse({'status': 'error', 'message': 'Email is required'}, status=500)
+
+
+# data examples
+# {u'positions': [u'export.script x 5'], u'type': [u'ext'], u'email': [u'sky-life@inbox.ru']}
+# {u'positions': [u'export.script'], u'type': [u'ext'], u'email': [u'sky-life@inbox.ru']}
+class PaymentForOfflineScript(View, GetproffExternalPayment):
+    PRODUCT_ID = 'export.script'
+
+    def get(self, request, *args, **kwargs):
+        positions = request.GET.get('positions')
+        email = request.GET.get('email')
+        if email:
+            if positions and self.PRODUCT_ID in positions:
+                products_count = self.get_products_count(positions)
+                for i in range(products_count):
+                    UserOfflineScriptExportAccess.objects.create(
+                        user=CustomUser.objects.get(username=email),
+                        payed=datetime.datetime.now()
+                    )
+                return JSONResponse({'status': 'success'}, status=200)
+            return JSONResponse({'status': 'error', 'message': 'Positions does not support with this method'}, status=500)
+        return JSONResponse({'status': 'error', 'message': 'Email is required'}, status=500)
+
+
+# data examples
+# {u'positions': [u'export.script.unlim.12'], u'type': [u'ext'], u'email': [u'sky-life@inbox.ru']}
+# {u'positions': [u'export.script.unlim'], u'type': [u'ext'], u'email': [u'sky-life@inbox.ru']}
+class PaymentForUnlimitedOfflineScript(View, GetproffExternalPayment):
+    PRODUCT_ID = 'export.script.unlim'
+
+    def get(self, request, *args, **kwargs):
+        positions = request.GET.get('positions')
+        email = request.GET.get('email')
+        if email:
+            if positions and self.PRODUCT_ID in positions:
+                months = self.get_multiplier(positions)
+                UserOfflineScriptExportAccess.objects.create(
+                    user=CustomUser.objects.get(username=email),
+                    payed=datetime.datetime.now(),
+                    unlim_months=months
+                )
+                return JSONResponse({'status': 'success'}, status=200)
+            return JSONResponse({'status': 'error', 'message': 'Positions does not support with this method'}, status=500)
+        return JSONResponse({'status': 'error', 'message': 'Email is required'}, status=500)
